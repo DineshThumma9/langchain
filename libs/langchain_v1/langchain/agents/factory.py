003fce39a5374832148ee3a76ce8ddb9dd5843d8
Thumma Dinesh <dineshthumma15@gmail.com>
2026-01-17 18:59:18 +0530

    Fix: Handled UnboundedIndex Error in case AIMessage becomes None

--- libs/langchain_v1/langchain/agents/factory.py
+++ libs/langchain_v1/langchain/agents/factory.py
@@ -1515,14 +1515,17 @@ def _resolve_jump(
 def _fetch_last_ai_and_tool_messages(
     messages: list[AnyMessage],
 ) -> tuple[AIMessage, list[ToolMessage]]:
-    last_ai_index: int
-    last_ai_message: AIMessage
+    last_ai_index: int = -1
+    last_ai_message: AIMessage = AIMessage(content="")
 
     for i in range(len(messages) - 1, -1, -1):
         if isinstance(messages[i], AIMessage):
             last_ai_index = i
             last_ai_message = cast("AIMessage", messages[i])
             break
+
+    if last_ai_index == -1:
+        return AIMessage(content=""), []
 
     tool_messages = [m for m in messages[last_ai_index + 1 :] if isinstance(m, ToolMessage)]
     return last_ai_message, tool_messages
